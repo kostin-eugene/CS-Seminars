9b3b62c09a984ff8e4fee281901a88e2f2b3ec2d
kostin-eugene <114108901+kostin-eugene@users.noreply.github.com>
2022-10-07 13:26:19 +0300

    add Task6

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+if (a % 2 == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
