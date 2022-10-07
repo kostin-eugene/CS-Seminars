7717feaff3a49e7f1b11cb9ad46d9b1c848dad32
kostin-eugene <114108901+kostin-eugene@users.noreply.github.com>
2022-10-07 13:57:39 +0300

    Tasks for seminar1

---
+++ Seminar1/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿int a, b;
+Console.WriteLine("Введите первое число:");
+int.TryParse(Console.ReadLine()!, out a);
+Console.WriteLine("Введите второе число:");
+int.TryParse(Console.ReadLine()!, out b);
+if (a > b)
+    Console.WriteLine("max: {0}\n", a);
+else
+    Console.WriteLine("max: {0}\n", b);
+
---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, c;
+Console.WriteLine("Введите 3 числа:");
+int.TryParse(Console.ReadLine()!, out a);
+int.TryParse(Console.ReadLine()!, out b);
+int.TryParse(Console.ReadLine()!, out c);
+int max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine("Наибольшее число: {0}\n", max);
+Console.ReadLine();
