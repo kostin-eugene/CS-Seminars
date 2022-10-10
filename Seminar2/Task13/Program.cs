a0b48775df3f190d152ed017be8c7499b60cc662
kostin-eugene <114108901+kostin-eugene@users.noreply.github.com>
2022-10-10 17:43:13 +0300

    Add seminar2 Tasks

---
+++ Seminar2/Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿int a;
+int.TryParse(Console.ReadLine()!, out a);
+int count = a.ToString().Length;
+Console.Write(MakeArray(a, count));
+int MakeArray(int a, int b)
+{
+    int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры не существует");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+    return result;
+}
---
+++ Seminar2/Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿int dayNumber;
+int.TryParse(Console.ReadLine()!, out dayNumber);
+if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("этот день выходной");
+}
+else if (dayNumber < 1 || dayNumber > 7)
+{
+    Console.WriteLine("Вводить нужно от 1 до 7");
+}
+else Console.WriteLine("этот день не выходной");
+
