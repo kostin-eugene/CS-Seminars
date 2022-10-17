10491f8a225d99f38bf05d07bbbad2ae5e5df18a
kostin-eugene <114108901+kostin-eugene@users.noreply.github.com>
2022-10-17 22:54:22 +0300

    add seminar6 tasks

---
+++ Semina6/Task41/Program.cs
@@ -0,0 +1,53 @@
+﻿
+//Сам не смог решить до конца, в самом начале не смог придумать как должны вводиться данные: через запятую, пробел или еще как-нибудь. Смотрел решение на семинаре. Очень круто!
+List<string> list = new List<string>();
+
+string user = Console.ReadLine()!;
+user += " ";
+string temp = "";
+
+for (int i = 0; i < user.Length; i++)
+{
+    if (user[i] == '-' && IsDigit(user[i + 1]) && i < user.Length - 1)
+    {
+        if (temp != "")
+        {
+            list.Add(temp);
+            temp = "-";
+        }
+        else
+            temp = "-";
+    }
+    else if (IsDigit(user[i]))
+    {
+        temp += user[i];
+    }
+    else
+    {
+        if (temp != "")
+        {
+            list.Add(temp);
+            temp = "";
+        }
+    }
+}
+
+int k = 0;
+
+foreach (var item in list)
+{
+    if (int.Parse(item) > 0)
+        k++;
+}
+
+
+foreach (var item in list)
+    Console.Write($"{item} ");
+Console.Write($"-> {k}");
+
+bool IsDigit(char _ch)
+{
+    if (_ch >= '0' && _ch <= '9')
+        return true;
+    return false;
+}
---
+++ Semina6/Task43/Program.cs
@@ -0,0 +1,14 @@
+﻿double b1, b2, k1, k2;
+Console.WriteLine("введите значение b1");
+double.TryParse(Console.ReadLine()!, out b1);
+Console.WriteLine("введите число k1");
+double.TryParse(Console.ReadLine()!, out k1);
+Console.WriteLine("введите значение b2");
+double.TryParse(Console.ReadLine()!, out b2);
+Console.WriteLine("введите число k2");
+double.TryParse(Console.ReadLine()!, out k2);
+
+double X = (b2 - b1) / (k1 - k2);
+double Y = k2 * X + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: {X},{Y}");
